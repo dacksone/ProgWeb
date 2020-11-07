12c1595c4a96dad731be0578dd12c7e78bc3bf12
Juan Rodriguez <63132602+dacksone@users.noreply.github.com>
2020-11-07 11:04:31 -0500

    Se agrega tabla a css y php

--- JavaScript/ImpElpleado.php
+++ JavaScript/ImpElpleado.php
@@ -39,52 +39,32 @@
       <fieldset>
 
                
-
+      <table >
+      
         <!-- Campo nombre -->
-        <label for="nombre">Nombre: <?php  echo $nombre; ?></label>
+        <tr> <td> <label for="nombre">Nombre:</td><td> <?php  echo $nombre; ?></label> </td> <tr>
        
-        <!-- <input type="text" id="nombre" name=""-->
-
-        <!-- Campo apellido -->
-        <label for="apellido">Apellido: <?php  echo $apellido; ?> </label>
-         <!-- <input type="text" id="apellido" name="apellido">
-
-        <!-- Campo cedula -->
-        <label for="cedula">Cedula: <?php  echo $cedula; ?></label>
-        <!--<input placeholder="Ingrese solo numeros"  type="text" id="cedula" name="cedula"> -->
-
-        <!-- Campo genero -->
-        <label for="genero">Genero: <?php  echo $genero; ?></label>
-        <!--<input type="radio" id="genero" value="Masculino" name="genero"><label for="under_13" class="light">Masculino</label><br>  -->
-        <!--<input type="radio" id="genero" value="Femenino" name="genero"><label for="over_13" class="light">Femenino</label><br> -->
-
-        <!-- Campo fecha Ingreso -->
-        <label for="fechaingreso">Fecha de ingreso: <?php  echo $fechaingreso; ?></label>
-       <!-- <input type="date" id="fechaingreso" name="fechaingreso"> -->
-
-        <!-- Campo sueldo basico -->
-        <label for="sueldobasico">Sueldo basico: <?php  echo $sueldobasico; ?></label>
-        <!--<input placeholder="Ingrese el valor numerico sin . o ," type="text" id="sueldobasico" name="sueldobasico"  > -->
-        <!--<input placeholder="Ingrese el valor numerico sin . o ," type="text" id="sueldobasico" name="sueldobasico" onChange="calculoSubTransporte(this.value,subsidiotransporte);" > -->
-        <!-- Campo numero de hijos -->
-        <label for="numerohijos">Numero de hijos: <?php  echo $numerohijos; ?></label>
-        <!--<input placeholder="Ingrese el valor numerico" type="text" id="numerohijos" name="numerohijos" onChange="calculoSubFamiliar(this.value,subsidiofamiliar);" > -->
-
-        <!-- Campo subisidio de transporte -->
-        <label for="subsidiotransporte">Subsidio de transporte: <?php  echo $subsidiotransporte; ?></label>
-       <!-- <input readonly  placeholder="campo de solo lectura" type="text" id="subsidiotransporte" name="subsidiotransporte" value=""> -->
-
-        <!-- Campo subisidio familiar -->
-        <label for="subsidiofamiliar">Subsidio familiar: <?php  echo $subsidiotransporte; ?></label>
-       <!-- <input readonly placeholder="campo de solo lectura" type="text" id="subsidiofamiliar" name="subsidiofamiliar" value=""> -->
-
-        <!-- Campo total devengado -->
-        <label for="totaldevengado">Total devengado: <?php  echo $totaldevengado; ?></label>
-       <!-- <input readonly placeholder="campo de solo lectura" type="text" id="totaldevengado" name="totaldevengado" value=""> -->
-
+        
+        <tr> <td> <label for="nombre">Apellido:</td><td> <?php  echo $apellido; ?></label> </td> <tr>
+        
+        <tr> <td> <label for="nombre">Cedula:</td><td> <?php  echo $cedula; ?></label> </td> <tr>
+        
+        <tr> <td> <label for="nombre">Genero:</td><td> <?php  echo $genero; ?></label> </td> <tr>
+        
+        <tr> <td> <label for="nombre">Fecha de ingreso:</td><td> <?php  echo $fechaingreso; ?></label> </td> <tr>
+                     
+        <tr> <td> <label for="nombre">Numero de hijos:</td><td> <?php  echo $numerohijos; ?></label> </td> <tr>
+      
+        <tr> <td> <label for="nombre">Subsidio de transporte:</td><td> <?php  echo $subsidiotransporte; ?></label> </td> <tr>
+       
+        <tr> <td> <label for="nombre">Subsidio familiar:</td><td> <?php  echo $subsidiofamiliar; ?></label> </td> <tr>
+                     
+        <tr> <td> <label for="nombre">Total devengado:</td><td> <?php  echo $totaldevengado; ?></label> </td> <tr>
+       
       </fieldset>
 
-      
+      </table >
+      <br>
       <a href="RegEmpleado.html"><button>Volver</button></a>
 </div>
     <!-- Fin formulario -->
